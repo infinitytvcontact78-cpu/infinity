c5311ad9ba6889958dc8afd31b26cc1e3cd65c69
infinitytvcontact78-cpu <infinitytvcontact78@gmail.com>
2025-10-19 22:32:49 +0100

    Initial commit

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,7 @@ export default function Pricing() {
     {
       title: "3 Months",
       badge: "For 1 Screen",
-      price: "£25.00",
+      price: "€20.00",
       highlight: false,
       features: [
         "SD/HD/Full HD/4K quality",
@@ -23,7 +23,7 @@ export default function Pricing() {
     {
       title: "6 Months",
       badge: "For 1 Screen",
-      price: "£38.00",
+      price: "€38.00",
       highlight: false,
       features: [
         "SD/HD/Full HD/4K quality",
